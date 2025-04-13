ec3b088a0f288dfafe074506bba9c857b0dbef9b
dev.soheil <dev.soheil98@gmail.com>
2025-04-13 06:37:53 -0700

    Add api register route

--- routes/api/api.php
+++ routes/api/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'this is api route test';
-});
+
+Route::post('register', [UserController::class, 'register']);
