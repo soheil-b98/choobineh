dfc22addff45e7627486a154a668f654e097fdba
dev.soheil <dev.soheil98@gmail.com>
2025-04-13 10:11:11 -0700

    Feat Login logic(with sanctum)

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\LoginRequest;
 use App\Http\Requests\User\RegisterUserRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class UserController extends Controller
 {
@@ -17,15 +19,33 @@ class UserController extends Controller
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
-                'password' => Hash::make($request->pasword),
+                'password' => Hash::make($request->password),
                 'phone_number' => $request->phone_number,
                 'active' => false,
-                'role' => 'marketer',
             ]);
             return $this->success('User created successfully', $user, 200);
 
         } catch (\Exception $e) {
             return $this->error($e->getMessage(), $request->all(), 400);
+        }
+    }
+
+    function login(LoginRequest $request): JsonResponse
+    {
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return $this->error('invalid credentials', [], 400);
+            }
+
+            $user->activeUser();
+            $user->addToken();
+
+            return $this->success('User logged in successfully', $user, 200);
+
+        } catch (\Exception $e) {
+            return $this->error('', $e->getMessage(), 400);
         }
     }
 
