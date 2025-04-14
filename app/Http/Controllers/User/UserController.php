404499b160b689cd55e51f2c8b584d5d7286c999
dev.soheil <dev.soheil98@gmail.com>
2025-04-14 01:33:29 -0700

    Add logout function Fix UserController warnings: like undefined method warn and magic method(both warn coming from phpstorm)

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\User\LoginRequest;
 use App\Http\Requests\User\RegisterUserRequest;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Testing\Fluent\Concerns\Has;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -16,36 +17,46 @@ class UserController extends Controller
     function register(RegisterUserRequest $request): JsonResponse
     {
         try {
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-                'phone_number' => $request->phone_number,
+            $user = User::query()->create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+                'phone_number' => $request->input('phone_number'),
                 'active' => false,
             ]);
-            return $this->success('User created successfully', $user, 200);
+            return $this->success('User created successfully', $user);
 
-        } catch (\Exception $e) {
-            return $this->error($e->getMessage(), $request->all(), 400);
+        } catch (Exception $e) {
+            return $this->error($e->getMessage(), $request->all());
         }
     }
 
     function login(LoginRequest $request): JsonResponse
     {
         try {
-            $user = User::where('email', $request->email)->first();
+            $user = User::query()->where('email', $request->input('email'))->first();
 
-            if (!$user || !Hash::check($request->password, $user->password)) {
-                return $this->error('invalid credentials', [], 400);
+            if (!$user || !Hash::check($request->input('password'), $user->password)) {
+                return $this->error('invalid credentials');
             }
 
             $user->activeUser();
             $user->addToken();
 
-            return $this->success('User logged in successfully', $user, 200);
+            return $this->success('User logged in successfully', $user);
 
-        } catch (\Exception $e) {
-            return $this->error('', $e->getMessage(), 400);
+        } catch (Exception $e) {
+            return $this->error('', $e->getMessage());
+        }
+    }
+
+    function logout(Request $request): JsonResponse
+    {
+        try {
+            $request->user()->tokens()->delete();
+            return $this->success('User logged out successfully');
+        } catch (Exception $e) {
+            return $this->error($e->getMessage());
         }
     }
 
